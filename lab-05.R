a00657de9b496dd61879995a154c1e59af839bdd
rnkulkar <rnkulkar@ucsc.edu>
2022-02-18 15:07:49 -0800

    Finished Lab 5!

--- lab-05.R
+++ lab-05.R
@@ -10,6 +10,8 @@ glimpse(airbnb)
 
 cleaning_fee <- airbnb$price * 0.02
 
+airbnb$cleaning_fee <- cleaning_fee
+
 hist(cleaning_fee)
 summary(cleaning_fee)
 
@@ -26,3 +28,27 @@ table(airbnb$minimum_nights)
 
 
 airbnb <- filter(airbnb, minimum_nights <= 3)
+
+length(cleaning_fee)
+length(airbnb$price)
+
+price_3_nights <- airbnb$price + airbnb$cleaning_fee
+airbnb$price_3_nights <- price_3_nights
+
+model <- lm(price_3_nights ~ neighbourhood + number_of_reviews + reviews_per_month, 
+            data=airbnb, na.action=NULL)
+summary(model)
+
+predict(model, data.frame(neighbourhood=c("City of Santa Cruz"), number_of_reviews=c(10),
+                          reviews_per_month=c(5.14)), interval="prediction"
+            )
+
+glimpse(airbnb)
+
+res <- resid(model)
+
+hist(res)
+
+
+
+
